ab1a327d4698e22febae9710841a828fc28f690a
lgj <lgj@lgj.com>
2022-03-13 16:31:02 +0800

    practice

--- threads/mutex2.c
+++ threads/mutex2.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define NHASH 29
+#define NHASH 9
 #define HASH(id) (((unsigned long)id)%NHASH)
 
 struct foo *fh[NHASH];
@@ -30,6 +30,7 @@ foo_alloc(int id) /* allocate the object */
 			return(NULL);
 		}
 		idx = HASH(id);
+		printf("id=%d, idx(HASH(id))=%d\n",id, idx);
 		pthread_mutex_lock(&hashlock);
 		fp->f_next = fh[idx];
 		fh[idx] = fp;
@@ -41,16 +42,16 @@ foo_alloc(int id) /* allocate the object */
 	return(fp);
 }
 
-void
-foo_hold(struct foo *fp) /* add a reference to the object */
+void foo_hold(struct foo *fp) /* add a reference to the object */
 {
 	pthread_mutex_lock(&fp->f_lock);
 	fp->f_count++;
+	printf("hold lock: f_count=%d\n",fp->f_count++);
+	//do something while holding this lock
 	pthread_mutex_unlock(&fp->f_lock);
 }
 
-struct foo *
-foo_find(int id) /* find an existing object */
+struct foo* foo_find(int id) /* find an existing object */
 {
 	struct foo	*fp;
 
@@ -64,9 +65,8 @@ foo_find(int id) /* find an existing object */
 	pthread_mutex_unlock(&hashlock);
 	return(fp);
 }
-
-void
-foo_rele(struct foo *fp) /* release a reference to the object */
+//fp must be in fp
+void foo_rele(struct foo *fp) /* release a reference to the object */
 {
 	struct foo	*tfp;
 	int			idx;
@@ -83,11 +83,11 @@ foo_rele(struct foo *fp) /* release a reference to the object */
 			pthread_mutex_unlock(&hashlock);
 			return;
 		}
-		/* remove from list */
+		/* remove fp from list */
 		idx = HASH(fp->f_id);
 		tfp = fh[idx];
 		if (tfp == fp) {
-			fh[idx] = fp->f_next;
+			fh[idx] = fp->f_next; 
 		} else {
 			while (tfp->f_next != fp)
 				tfp = tfp->f_next;
@@ -101,4 +101,45 @@ foo_rele(struct foo *fp) /* release a reference to the object */
 		fp->f_count--;
 		pthread_mutex_unlock(&fp->f_lock);
 	}
+}
+
+void hash_print(){
+	for(int i=0;i<NHASH;i++){
+		printf("[%.2d]:",i);
+		if(fh[i]==NULL){
+			printf("<NULL>\n");
+		}
+		else{
+			struct foo* temp=fh[i];
+			while(temp){
+				printf("<%x: %d>",temp,temp->f_id);
+				if(temp->f_next){
+					printf("-->");
+				}else{
+					printf("\n");
+					break;
+				}
+				temp=temp->f_next;
+			}
+		}
+	}
+
+}
+
+int main(){
+	foo_alloc(1);
+	foo_alloc(10);
+	foo_alloc(19);
+	struct foo* temp;
+	hash_print();
+	
+/*
+	if((temp=foo_find(1))==NULL){
+		printf("cannot find an existing object for id=%d\n",1);
+	}
+	else{
+		printf("find an existing object for id=%d at addr=%x\n ",1,temp);
+	}
+*/	
+	return 0;
 }
